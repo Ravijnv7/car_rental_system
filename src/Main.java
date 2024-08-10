6e1c9985ed2e6dd6616b72f89bcd4ff7a2a120ea
Ravijnv7 <manohar0444@gmail.com>
2024-08-10 13:36:54 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+
+    class Car{
+        private String car_name;
+        private String car_brand;
+        private boolean isAvailable;
+
+        Car(String car_name,String car_brand){
+            this.car_name=car_name;
+            this.car_brand=car_brand;
+            isAvailable=true;
+        }
+        class Customer{
+
+        }
+
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
